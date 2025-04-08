4a3f837d9d532f4f807ef4be8c31ae81205d707a
Dries Van De Putte <dvandepu@uwo.ca>
2025-04-08 16:27:15 -0400

    Templates for all combinations of wcscorr/notcorr stitch/nostitch

--- shell_scripts/postprocess_nirspec.bash
+++ shell_scripts/postprocess_nirspec.bash
@@ -50,8 +50,11 @@ done
 # WCS correction needs to happen here
 python3 -m pdrs4all.postprocess.nirspec_wcs_calibrate_stitch cubes/default/*s3d.fits --output_dir cubes/default_wcscorr
 
-# templates from default cubes (no wcs corr)
-extract_templates "$ROOT"/regions/aper_T_DF_extraction.reg cubes/default/*s3d.fits --template_names "HII" "Atomic" "DF3" "DF2" "DF1" -o templates/default.ecsv
+# templates from both cubes, both stitched and not stitched
+extract_templates "$ROOT"/regions/aper_T_DF_extraction.reg cubes/default/*s3d.fits --template_names "HII" "Atomic" "DF3" "DF2" "DF1" -o templates/default_nostitch.ecsv
 
-# templates from wcs corrected cube
-extract_templates "$ROOT"/regions/aper_T_DF_extraction.reg cubes/default_wcscorr/nirspec_naive_stitch_wcscorr_s3d.fits --template_names "HII" "Atomic" "DF3" "DF2" "DF1" -o templates/default_wcscorr.ecsv
+extract_templates "$ROOT"/regions/aper_T_DF_extraction.reg cubes/default/*s3d.fits --template_names "HII" "Atomic" "DF3" "DF2" "DF1" --apply_offsets --reference_segment 2 -o templates/default_addstitch.ecsv
+
+extract_templates "$ROOT"/regions/aper_T_DF_extraction.reg cubes/default_wcscorr/nirspec_naive_stitch_wcscorr_s3d.fits --template_names "HII" "Atomic" "DF3" "DF2" "DF1" -o templates/default_wcscorr_nostitch.ecsv
+
+extract_templates "$ROOT"/regions/aper_T_DF_extraction.reg cubes/default_wcscorr/nirspec_naive_stitch_wcscorr_s3d.fits --template_names "HII" "Atomic" "DF3" "DF2" "DF1" --apply_offsets --reference_segment 2 -o templates/default_wcscorr_addstitch.ecsv
